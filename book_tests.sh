9ffbc42cd2258c1a57a6a54afd0e473787ba832f
Manuel Ceron <ceronman@gmail.com>
2024-08-23 17:07:03 +0200

    Added a script to run book tests

---
+++ book_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TEST_RUNNER=../writing-a-c-compiler-tests/test_compiler
+
+set -euxo pipefail
+  cargo build
+  arch -x86_64 $TEST_RUNNER target/debug/writing-a-c-compiler --chapter 1
