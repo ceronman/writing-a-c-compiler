cdadc70f518c0d818557bf0d0b6d569643586c74
Manuel Ceron <ceronman@gmail.com>
2025-05-18 21:51:29 +0200

    Added lexer support for chapter 15

--- generate_tests.sh
+++ generate_tests.sh
@@ -5,9 +5,9 @@ TEST_RUNNER=../writing-a-c-compiler-tests/test_compiler
 set -euxo pipefail
 cargo build --features test_gen
 
-#cp /dev/null src/lexer/test.rs
-#arch -x86_64 $TEST_RUNNER target/debug/writing-a-c-compiler --verbose --chapter 14 --stage lex --bitwise --compound --increment --goto --switch --nan
-#rustfmt src/lexer/test.rs
+cp /dev/null src/lexer/test.rs
+arch -x86_64 $TEST_RUNNER target/debug/writing-a-c-compiler --verbose --chapter 15 --stage lex --bitwise --compound --increment --goto --switch --nan
+rustfmt src/lexer/test.rs
 
 #cp /dev/null src/parser/test.rs
 #arch -x86_64 $TEST_RUNNER target/debug/writing-a-c-compiler --verbose --chapter 14 --stage parse --bitwise --compound --increment --goto --switch --nan
@@ -17,8 +17,8 @@ cargo build --features test_gen
 #arch -x86_64 $TEST_RUNNER target/debug/writing-a-c-compiler --verbose --chapter 14 --stage validate --bitwise --compound --increment --goto --switch --nan
 #rustfmt src/semantic/test.rs
 #
-cp /dev/null src/tacky/test.rs
-arch -x86_64 $TEST_RUNNER target/debug/writing-a-c-compiler --verbose --chapter 14 --stage tacky --bitwise --compound --increment --goto --switch --nan
-rustfmt src/tacky/test.rs
+#cp /dev/null src/tacky/test.rs
+#arch -x86_64 $TEST_RUNNER target/debug/writing-a-c-compiler --verbose --chapter 14 --stage tacky --bitwise --compound --increment --goto --switch --nan
+#rustfmt src/tacky/test.rs
 
 cargo clean
